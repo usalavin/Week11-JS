330568976efaf9c8fbf6b2261654204bedf44aac
usalavin@tktk.ee <usalavin@tktk.ee>
2022-12-17 13:57:02 -0800

    JS-Week11

---
+++ js/randomImage.js
@@ -0,0 +1,11 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector ('img');
+const images = ["pic1.jpeg", "pic2.jpeg", "pic3.jpeg", "pic4.jpeg"];
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage() {
+
+    let randomIndex = Math.floor(Math.random()* images.length);
+    image.src ="img/" + images[randomIndex]
+}
